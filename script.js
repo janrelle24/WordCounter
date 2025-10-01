f53541c5dce35de2bf015da058a85915f7987ddf
janrelle24 <lubianojanrelle@gmail.com>
2025-10-01 09:12:37 +0800

    add features like changing font-style and font-size

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const textInput = document.getElementById("input");
 const wordCountDisplay = document.getElementById("count");
+const fontSelect = document.getElementById("font-select");
+const fontSizeSelect = document.getElementById("fontSize-select");
 
 function wordCounts(text){
     const trimmed = text.trim();
@@ -11,5 +13,11 @@ textInput.addEventListener("input", () =>{
     const count = wordCounts(textInput.value);
     
     wordCountDisplay.textContent = count;
-    
 });
+
+fontSelect.addEventListener("change", () =>{
+    textInput.style.fontFamily = fontSelect.value;
+});
+fontSizeSelect.addEventListener("change", () => {
+    textInput.style.fontSize = fontSizeSelect.value;
+})
